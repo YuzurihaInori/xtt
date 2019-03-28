b45371321c82154c39515f6566720df85cac3f1d
chen <823130614@qq.com>
2019-03-28 15:37:59 +0800

    fix

--- app/screen/main/Home.js
+++ app/screen/main/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import { AppRegistry, StyleSheet, Text, View, FlatList, Image, TouchableOpacity,ImageBackground } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, FlatList, Image, TouchableOpacity, ImageBackground, Dimensions } from 'react-native';
 
 import Swiper from 'react-native-swiper';
+
+
+const screenW = Dimensions.get('window').width;
+const width = 165;
+const space = (screenW - width * 2) / 3 / 2;
 
 class Head extends React.Component {
 
@@ -27,23 +32,24 @@ class Head extends React.Component {
 
 class Item extends React.Component {
 
-  _itemPressed(){
+  _itemPressed() {
     alert("pressed")
   }
 
   render() {
     return (
       <TouchableOpacity style={styles.item_container} onPress={this._itemPressed}>
-          <ImageBackground style={styles.item_img}
-            source={{ uri: this.props.chatIcon }} >
-            
-              <ImageBackground style={{height:14,position:"absolute",bottom:0,left:0,right:0}} source={require("../../image/chatting_item_list_bg.png")}>
-                <Text style={{fontSize:10,color:"#ffffff",textAlignVertical:"center"}}>{this.props.chatOnlineNum}人在线</Text>
-              </ImageBackground>
-            </ImageBackground>
-          <Text style={styles.item_title_text}>{this.props.chatName}</Text>
+        <ImageBackground style={styles.item_img}
+          source={{ uri: this.props.chatIcon }} >
 
-          <Text style={styles.item_content_text}>{this.props.chatHostNick}</Text>
+          <ImageBackground style={{ height: 14, position: "absolute", bottom: 0, left: 0, right: 0, borderBottomLeftRadius: 8, borderBottomRightRadius: 8 }}
+            source={require("../../image/chatting_item_list_bg.png")}>
+            <Text style={{ fontSize: 10, color: "#ffffff", textAlignVertical: "center", marginLeft: 6 }}>{this.props.chatOnlineNum}人在线</Text>
+          </ImageBackground>
+        </ImageBackground>
+        <Text style={styles.item_title_text}>{this.props.chatName}</Text>
+
+        <Text style={styles.item_content_text}>{this.props.chatHostNick}</Text>
 
       </TouchableOpacity>
 
@@ -52,25 +58,57 @@ class Item extends React.Component {
 }
 
 
-let data = require('../../test/homeData.json');
-
 export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+      data:[],
+    }
+  }
+  componentDidMount(){
+    this._loadData();
+  }
+
   render() {
+
+
     return (
       <View style={styles.container}>
         <Text style={styles.title_text}>小邂逅</Text>
         <FlatList
+          refreshing ={this.state.refreshing}
+          onRefresh={this._onRefresh.bind(this)}
+          columnWrapperStyle={{ marginLeft: space, marginRight: space }}
           ListHeaderComponent={<Head />}
-          data={data.data.list}
+
+          data={this.state.data?.data?.list}
+          // data={require('../../test/homeData.json').data.list}
           numColumns={2}
           renderItem={
             ({ item }) =>
-              <Item chatIcon={item.chat_room_icon} chatName={item.chat_room_name} chatHostNick={item.chat_host_nick} chatOnlineNum={item.chat_online_num}/>
+              <Item chatIcon={item.chat_room_icon} chatName={item.chat_room_name} chatHostNick={item.chat_host_nick} chatOnlineNum={item.chat_online_num} />
           }
         />
       </View>
     );
   }
+
+
+  _loadData() {
+    this.setState({
+      data : require('../../test/homeData.json'),
+      refreshing : false,
+    })
+  }
+
+  _onRefresh() {
+    if (!this.state.isRefresh) {
+      this._loadData();
+    }
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -88,6 +126,7 @@ const styles = StyleSheet.create({
 
   wrapper: {
     height: 160,
+    marginBottom: 12,
   },
   slide1: {
     flex: 1,
@@ -122,30 +161,36 @@ const styles = StyleSheet.create({
   },
 
   item_container: {
-    flex: 1,
-    alignItems:"flex-start",
+    flex: 0.5,
+    flexDirection: "column",
+    borderLeftWidth: space,
+    borderRightWidth: space,
+    borderColor: '#00000000'
   },
 
   item_img: {
     width: 165,
     height: 165,
-
+    borderRadius: 8,
+    alignSelf: "center",
+    overflow: 'hidden',
   },
   item_title_text: {
-    flex:1,
+    alignSelf: "flex-start",
     color: '#222222',
     fontSize: 12,
     marginTop: 8,
-    justifyContent:"flex-start",
+    textAlign: "left",
+    marginLeft: 8,
     textAlignVertical: 'center',
   },
   item_content_text: {
-    flex:1,
+    alignSelf: "flex-start",
     color: '#aaaaaa',
     fontSize: 10,
     marginTop: 4,
-    justifyContent:"flex-start",
-    marginBottom:8,
+    marginLeft: 8,
+    marginBottom: 8,
     textAlignVertical: 'center',
   },
 })
